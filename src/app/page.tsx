4b6b409f8fe1f05f0d9fdce545db60b93c2525d1
wallpants <47203170+wallpants@users.noreply.github.com>
2024-06-17 18:42:34 -0600

    finish landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Icons } from "@/components/icons";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { Phone } from "@/components/phone";
-import { CheckIcon, StarIcon } from "lucide-react";
+import { Reviews } from "@/components/reviews";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon, CheckIcon, StarIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
    return (
@@ -108,6 +112,164 @@ export default function Home() {
                      <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
                   </div>
                </div>
+            </MaxWidthWrapper>
+         </section>
+
+         <section className="bg-slate-100 py-24">
+            <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+               <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+                  <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                     What our{" "}
+                     <span className="relative px-2">
+                        customers{" "}
+                        <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+                     </span>{" "}
+                     say
+                  </h2>
+                  {/* eslint-disable-next-line */}
+                  <img src="/snake-2.png" alt="snake" className="w-24 order-0 lg:order-2" />
+               </div>
+
+               <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+                  <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                     <div className="flex gap-0.5 mb-2">
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                     </div>
+                     <div className="text-lg leading-8">
+                        <p>
+                           &quot;The case feels durable and I even got a compliment on the design.
+                           Had the case for two and a half months{" "}
+                           <span className="p-0.5 bg-slate-800 text-white">
+                              the image is super clear.
+                           </span>{" "}
+                           On the case I had before, the image started fading into yellow-ish color
+                           after a couple weeks. Love it.&quot;
+                        </p>
+                     </div>
+
+                     <div className="flex gap-4 mt-2">
+                        {/* eslint-disable-next-line */}
+                        <img
+                           src="/users/user-1.png"
+                           className="rounded-full h-12 w-12 object-cover"
+                           alt="user"
+                        />
+                        <div className="flex flex-col">
+                           <p className="font-semibold">Jonathan</p>
+                           <div className="flex gap-1.5 items-center text-zinc-600">
+                              <CheckIcon className="h-4 w-4 stroke-[3px] text-green-600" />
+                              <p className="text-sm">Verified purchase</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                     <div className="flex gap-0.5 mb-2">
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                        <StarIcon className="h-5 w-5 text-green-600 fill-green-600" />
+                     </div>
+                     <div className="text-lg leading-8">
+                        <p>
+                           &quot;I usually keep my phone together with my keys in my pocket and that
+                           led to some pretty heavy scratch marks in all of my last phone cases.
+                           This one, besides a barely noticeably scratch on the corner,{" "}
+                           <span className="p-0.5 bg-slate-800 text-white">
+                              looks brand new after about half a year.
+                           </span>{" "}
+                           I dig it.&quot;
+                        </p>
+                     </div>
+
+                     <div className="flex gap-4 mt-2">
+                        {/* eslint-disable-next-line */}
+                        <img
+                           src="/users/user-4.jpg"
+                           className="rounded-full h-12 w-12 object-cover"
+                           alt="user"
+                        />
+                        <div className="flex flex-col">
+                           <p className="font-semibold">Gual</p>
+                           <div className="flex gap-1.5 items-center text-zinc-600">
+                              <CheckIcon className="h-4 w-4 stroke-[3px] text-green-600" />
+                              <p className="text-sm">Verified purchase</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </MaxWidthWrapper>
+
+            <div className="pt-16">
+               <Reviews />
+            </div>
+         </section>
+
+         <section>
+            <MaxWidthWrapper className="py-24">
+               <div className="mb-12 px-6 lg:px-8">
+                  <div className="mx-auto max-w-2xl sm:text-center">
+                     <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                        Upload your photo and get{" "}
+                        <span className="relative px-2 bg-green-600 text-white">your own case</span>{" "}
+                        now
+                     </h2>
+                  </div>
+               </div>
+
+               <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                  <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+                     {/* eslint-disable-next-line */}
+                     <img
+                        src="/arrow.png"
+                        className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+                     />
+
+                     <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                        {/* eslint-disable-next-line */}
+                        <img
+                           src="/horse.jpg"
+                           className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                        />
+                     </div>
+
+                     <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+                  </div>
+               </div>
+
+               <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+                  <li className="w-fit">
+                     <CheckIcon className="h-5 w-5 text-green-600 inline mr-1.5" />
+                     High-quality silicone material
+                  </li>
+                  <li className="w-fit">
+                     <CheckIcon className="h-5 w-5 text-green-600 inline mr-1.5" />
+                     Scratch- and fingerprint resistant coating
+                  </li>
+                  <li className="w-fit">
+                     <CheckIcon className="h-5 w-5 text-green-600 inline mr-1.5" />
+                     Wireless charging compatible
+                  </li>
+                  <li className="w-fit">
+                     <CheckIcon className="h-5 w-5 text-green-600 inline mr-1.5" />5 year print
+                     warranty
+                  </li>
+
+                  <div className="flex justify-center">
+                     <Button size="lg" className="mx-auto mt-8" asChild>
+                        <Link href="/configure/upload">
+                           Create your case now <ArrowRightIcon className="h-4 w-4 ml-1.5" />
+                        </Link>
+                     </Button>
+                  </div>
+               </ul>
             </MaxWidthWrapper>
          </section>
       </div>
